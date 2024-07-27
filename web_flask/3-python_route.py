b8353d5d0813559426e1d929c3386337ab304806
root <toqaezzatly@gmail.com>
2024-07-27 10:49:09 -0700

    hey

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,8 +4,8 @@ Flask web application that listens on 0.0.0.0, port 5000
 Routes:
 /: display “Hello HBNB!”
 /hbnb: display “HBNB”
-/c/<text>: display “C ” followed `(replace underscore _ symbols with a space )
-/python/<text>: display “Python ”,....
+/c/<text>: display “C ” (replace underscore _ symbols with a space )
+/python/<text>: display “Python ”, ...
 """
 from flask import Flask, request
 
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Display 'Hello HBNB!'"""
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
@@ -30,9 +30,10 @@ def c_text(text):
     text = text.replace('_', ' ')
     return f"C {text}"
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_text(texti):
+def python_text(text):
     """Displays 'Python ' followed by the value of the text variable"""
     text = text.replace('_', ' ')
     return f"Python {text}"
