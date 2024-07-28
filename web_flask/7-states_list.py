98063e208888ef166d856fba361a44fd7a761bec
root <toqaezzatly@gmail.com>
2024-07-28 15:48:30 -0700

    hi

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,78 +1,25 @@
 #!/usr/bin/python3
 """
-Flask web application that listens on 0.0.0.0, port 5000
-Routes:
-/: display “Hello HBNB!”
-/hbnb: display “HBNB”
-/c/<text>: display “C ” (replace underscore _ symbols with a space )
-/python/<text>: display “Python ”, ...
-/number/<n>: display “n is a number” only if n is an integer
-/number_template/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n” inside the tag BODY
-/number_odd_or_even/<n>: display a HTML page only if n is an integer:
-H1 tag: “Number: n is even|odd” inside the tag BODY
+starts a Flask web application
 """
-from flask import Flask, request, render_template
-rom models import storage
-from models.state import State
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'"""
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """Displays 'C ' followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return f"C {text}"
-
-
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    """Displays 'Python ' followed by the value of the text variable"""
-    text = text.replace('_', ' ')
-    return f"Python {text}"
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """Displays 'n is a number' only if n is an integer"""
-    return f"{n} is a number"
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_templete(n):
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    parity = "even" if n % 2 == 0 else "odd"
-    return render_template('6-number_odd_or_even.html', n=n, parity=parity)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda state: state.name)
+    """display a HTML page with the states listed in alphabetical order"""
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
+
 @app.teardown_appcontext
-def teardown(exception):
+def teardown_db(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
